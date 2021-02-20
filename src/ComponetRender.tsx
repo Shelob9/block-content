6a7c9552402b7268b273edee2bb15c9c42d894ec
Josh Pollock <jpollock412@gmail.com>
2021-02-20 18:59:08 -0500

    array of components

--- src/ComponetRender.tsx
+++ src/ComponetRender.tsx
@@ -2,30 +2,35 @@ import React, { createElement, FC, Fragment, useMemo } from 'react';
 import { NODE } from '.';
 import { preProcessTagNode } from './preProcessNode';
 
-export interface ComponetsMap  {[key:string]: FC<{children:any,className:string}>};
-let defaultComponents : ComponetsMap = {
-  'p':({children,className}) => (
-    <p className={className}>{children}</p>
-  )
+export interface ComponetsMap {
+  [key: string]: FC<{ children: any; className: string }>;
 }
-const ComponetRender: FC<{ node: NODE,components?:ComponetsMap }> = (props) => {
-  const {node} = props;
+//Ideally this would be in a context provider
+let defaultComponents: ComponetsMap = {
+  p: ({ children, className }) => <p className={className}>{children}</p>,
+};
+const ComponetRender: FC<{ node: NODE; components?: ComponetsMap }> = props => {
+  const { node } = props;
   const components = useMemo(() => {
-    if( ! props.components ){
+    if (!props.components) {
       return defaultComponents;
     }
     return {
       ...defaultComponents,
       ...props.components,
-    }
-  }, [props.node]);
-  
+    };
+  }, [props.components]);
+
   return createElement(
-    node.name && components.hasOwnProperty(node.name) ? components[node.name] : node.name as string,
+    node.name && components.hasOwnProperty(node.name)
+      ? components[node.name]
+      : (node.name as string),
     {
       //@ts-ignore
       className:
-        node.attribs && node.attribs.class ? node.attribs.class as string : undefined,
+        node.attribs && node.attribs.class
+          ? (node.attribs.class as string)
+          : undefined,
     },
     node.children
       ? node.children.map((child: NODE, i: number) => {
@@ -34,9 +39,9 @@ const ComponetRender: FC<{ node: NODE,components?:ComponetsMap }> = (props) => {
               {'text' === child.type ? (
                 <Fragment>{child.data}</Fragment>
               ) : (
-                <ComponetRender 
+                <ComponetRender
                   components={components}
-                  node={preProcessTagNode(child)} 
+                  node={preProcessTagNode(child)}
                 />
               )}
             </Fragment>
